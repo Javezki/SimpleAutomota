839cb67400e58b6cf9970491cdc02f180033e03f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-10 13:27:09 -0400

    Empty cable class created

---
+++ src/main/java/io/github/javezki/items/cables/Cable.java
@@ -0,0 +1,12 @@
+package io.github.javezki.items.cables;
+
+import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
+
+/**
+ * The Default Cable class, used as the parent for cables. Contains energy net component
+ * as well as wrench interaction
+ */
+public abstract class Cable implements EnergyNetComponent {
+    
+
+}
